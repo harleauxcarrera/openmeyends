eb9a5a3bf33c69d60aadc2585a9d22aae5537d64
harleauxcarrera <harleauxcarrera@gmail.com>
2020-09-10 19:40:27 -0700

    added routing to menu item components. Fix /shop/route

--- src/components/menu-item/menu-items.component.jsx
+++ src/components/menu-item/menu-items.component.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
-import '../menu-item/menu-item.styles.scss';
- 
-export const MenuItem = ({title, imageUrl, size }) => {
-    return (
-        <div style={{
-          backgroundImage: `url(${imageUrl})` 
-        }} className={`${size} menu-item`}>
-          <div className="content">
-            <h1 className="title">{title}</h1>
-            <span className="subtitle">SHOP</span>
-          </div>
-        </div>
-    );
-}
-export default MenuItem; 
+import React from "react";
+import { withRouter } from "react-router-dom";
+import "../menu-item/menu-item.styles.scss";
+
+export const MenuItem = ({
+  title,
+  imageUrl,
+  size,
+  history,
+  linkUrl,
+  match,
+}) => {
+  //history accessed through withRouter
+  return (
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />
+      <div className="content">
+        <h1 className="title">{title}</h1>
+        <span className="subtitle">SHOP</span>
+      </div>
+    </div>
+  );
+};
+export default withRouter(MenuItem); //wrap withRouter to get access to match/history location props for backspace routing
