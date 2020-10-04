53c603dbacb25df468e2d8dca218256cc97f387f
harleauxcarrera <harleauxcarrera@gmail.com>
2020-10-04 15:54:24 -0700

    Shop displaying raw Collection data

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
-
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats page</h1>
-  </div>
-);
-
+import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}/>
-        <Route  path="/shop/hats" component={HatsPage}/>
+        <Route  path="/shop" component={ShopPage}/>
       </Switch>
       </div>
   );
